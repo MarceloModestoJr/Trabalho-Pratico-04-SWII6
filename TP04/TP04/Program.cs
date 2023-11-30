76a32b0a7ac03cbb96855ccc7922932f71a44316
Marcelo Modesto <103297117+MarceloModestoJr@users.noreply.github.com>
2023-11-30 18:50:37 -0300

    Add files via upload

---
+++ TP04/TP04/Program.cs
@@ -0,0 +1,34 @@
+using WebAPI_SDK.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Marcelo Modesto de Lima Junior CB3015823
+//Bruna Helena Silva Santos      CB3016111
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddTransient<AtividadesService>(s => new AtividadesService("http://localhost:5095/api", new HttpClient()));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Atividades}/{action=Index}/{id?}");
+
+app.Run();
---
+++ TP04/WebAPI/Controllers/AtividadesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+using WebAPI.Repositories;
+
+//Marcelo Modesto de Lima Junior CB3015823
+//Bruna Helena Silva Santos      CB3016111
+
+namespace WebAPI.Controllers
+{
+    [Route("api/atividades")]
+    [ApiController]
+    public class AtividadesController : Controller
+    {
+        private readonly AtividadesRepository _repository;
+
+        public AtividadesController(AtividadesRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Atividade atividade)
+        {
+            _repository.Create(atividade);
+            return Created("", new object { });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Find(int id)
+        {
+            var atividade = await _repository.GetById(id);
+            return Ok(atividade);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var atividades = await _repository.GetAll();
+            return Ok(atividades);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Atividade atividade)
+        {
+            _repository.Update(id, atividade);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            _repository.Delete(id);
+            return NoContent();
+        }
+    }
+}
